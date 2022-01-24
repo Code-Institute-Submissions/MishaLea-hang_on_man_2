6e302e1ae599fb3ae5ff59f51c4397277752af3a
MishaLea <mishafreestone@ymail.com>
2022-01-24 17:13:58 +0000

    Add comments for each stage of game

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
-# python code goes here
+# setup the word and hidden list
+
+# loop until either the player has won or lost
+
+    # display the current boards, guessed letters, and attempts remaining
+
+    # ask the player for a character
+
+    # if the player guessed correct. show all matched letters and print message
+
+    # if the player guessed wrong, print failure message and increment attempts
+
+    # if the player has won, print the a win message and stop looping
+
+    # if the player has lost, print falling and stop looping

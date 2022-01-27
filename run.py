cb2364e185f09b1fe9a5428386a197b5d5cf7dd9
MishaLea <mishafreestone@ymail.com>
2022-01-27 09:37:35 +0000

    Add function to iterate game if win/lose

--- run.py
+++ run.py
@@ -4,145 +4,158 @@
 """
 import random
 
-WORDBANK = ['tree', 'sun', 'ball', 'moon', 'earth', 'grass', 'world',
-            'advert', 'octodont', 'pharos', 'physics', 'reality',
-            'possession', 'cousin', 'aspect', 'analyst']
 
-
-class Hangman:
+def main():
     """
-        Class used to create the objects that are included in the below
-        variables and functions.
+        Function created to iterate over game if required by user.
     """
-    word = ""
-    incorrect_guesses = 0
-    guessed_letters = []
 
-    def start_game(self):
+    wordbank = ['tree', 'sun', 'ball', 'moon', 'earth', 'grass', 'world',
+                'advert', 'octodont', 'pharos', 'physics', 'reality',
+                'possession', 'cousin', 'aspect', 'analyst']
+
+    class Hangman:
         """
-            Method to start the game.
+            Class used to create the objects that are included in the below
+            variables and functions.
         """
-        print()
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Hang on, Man!\n")
-        print("Guess letters to fill out the word--you have 6 attempts!")
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print()
+        word = ""
+        incorrect_guesses = 0
+        guessed_letters = []
 
-        self.word = random.choice(WORDBANK)
+        def start_game(self):
+            """
+                Method to start the game.
+            """
+            print()
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print("Hang on, Man!\n")
+            print("Guess letters to fill out the word--you have 6 attempts!")
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            print()
 
-        while True:
-            # Check if the game has already been lost
-            if self.incorrect_guesses == 6:
-                print(f"You DIE! The word was '{self.word}' :(")
-                break
+            self.word = random.choice(wordbank)
 
-            # Check if the game has already been won
-            did_win = True
-            for letter in self.word:
-                if letter not in self.guessed_letters:
-                    did_win = False
+            while True:
+                # Check if the game has already been lost
+                if self.incorrect_guesses == 6:
+                    print(f"You DIE! The word was '{self.word}' :(")
                     break
-            if did_win:
-                # The player has won the game!
+
+                    # Check if the game has already been won
+                did_win = True
+                for letter in self.word:
+                    if letter not in self.guessed_letters:
+                        did_win = False
+                        break
+                if did_win:
+                    # The player has won the game!
+                    self.display_game_board()
+                    print("Well done, you've guessed the word!")
+                    break
+
+                # Display board and play the game
+                self.display_hangman()
                 self.display_game_board()
-                print("Well done, you've guessed the word!")
-                break
+                self.prompt_user()
 
-            # Display board and play the game
-            self.display_hangman()
-            self.display_game_board()
-            self.prompt_user()
-
-    def display_hangman(self):
-        """
-        Method to display the hanging man representing wrong guesses.
-        """
-        print()
-        print()
-        if self.incorrect_guesses == 0:
-            print("6 guesses allowed")
-            print("-----------------")
-            return
-        elif self.incorrect_guesses == 1:
-            print("5 guesses left")
-            print("-----------------")
-            print("         0       ")
-        elif self.incorrect_guesses == 2:
-            print("4 guesses left")
-            print("-----------------")
-            print("         0       ")
-            print("         |       ")
-        elif self.incorrect_guesses == 3:
-            print("3 guesses left")
-            print("-----------------")
-            print("         0       ")
-            print("         |       ")
-            print("        / \\      ")
-        elif self.incorrect_guesses == 4:
-            print("2 guesses left")
-            print("-----------------")
-            print("       \\ 0 /     ")
-            print("         |       ")
-            print("        / \\      ")
-        elif self.incorrect_guesses == 5:
-            print("Only 1 guess left!")
-            print("-----------------")
-            print("         |       ")
-            print("       \\ 0 /     ")
-            print("         |       ")
-            print("        / \\      ")
-        else:
-            print("------------------")
-            print("        _|_      ")
-            print("         |       ")
-            print("       \\ 0 /     ")
-            print("         |       ")
-            print("        / \\      ")
-        print()
-        print()
-
-    def display_game_board(self):
-        """
-        Method to display blank spaces and guessed letters.
-        """
-        # Build the current guessed word
-        game_board = ""
-        for character in self.word:
-            if character in self.guessed_letters:
-                game_board += " " + character + " "
+        def display_hangman(self):
+            """
+            Method to display the hanging man representing wrong guesses.
+            """
+            print()
+            print()
+            if self.incorrect_guesses == 0:
+                print("6 guesses allowed")
+                print("-----------------")
+                return
+            elif self.incorrect_guesses == 1:
+                print("5 guesses left")
+                print("-----------------")
+                print("         0       ")
+            elif self.incorrect_guesses == 2:
+                print("4 guesses left")
+                print("-----------------")
+                print("         0       ")
+                print("         |       ")
+            elif self.incorrect_guesses == 3:
+                print("3 guesses left")
+                print("-----------------")
+                print("         0       ")
+                print("         |       ")
+                print("        / \\      ")
+            elif self.incorrect_guesses == 4:
+                print("2 guesses left")
+                print("-----------------")
+                print("       \\ 0 /     ")
+                print("         |       ")
+                print("        / \\      ")
+            elif self.incorrect_guesses == 5:
+                print("Only 1 guess left!")
+                print("-----------------")
+                print("         |       ")
+                print("       \\ 0 /     ")
+                print("         |       ")
+                print("        / \\      ")
             else:
-                game_board += " _ "
-        print()
-        print(game_board)
-        print()
+                print("------------------")
+                print("        _|_      ")
+                print("         |       ")
+                print("       \\ 0 /     ")
+                print("         |       ")
+                print("        / \\      ")
+            print()
+            print()
 
-    def prompt_user(self):
-        # Ask the user to guess a letter
-        """
-        Method to prompt the user to guess a letter, and process the input.
-        """
-        letter = input(' ')
+        def display_game_board(self):
+            """
+            Method to display blank spaces and guessed letters.
+            """
+            # Build the current guessed word
+            game_board = ""
+            for character in self.word:
+                if character in self.guessed_letters:
+                    game_board += " " + character + " "
+                else:
+                    game_board += " _ "
+            print()
+            print(game_board)
+            print()
 
-        if len(letter) > 1 or (not letter.isalpha()):
-            print("Invalid input! Try again.")
-            return
+        def prompt_user(self):
+            # Ask the user to guess a letter
+            """
+            Method to prompt the user to guess a letter, and process the input.
+            """
+            letter = input(' ')
 
-        # Already guessed letters are invalid
-        if letter in self.guessed_letters:
-            print("You've already guessed that letter! Try again.")
-            return
+            if len(letter) > 1 or (not letter.isalpha()):
+                print("Invalid input! Try again.")
+                return
 
-        # Add the current guess to the list of guessed letters
-        self.guessed_letters += letter
+            # Already guessed letters are invalid
+            if letter in self.guessed_letters:
+                print("You've already guessed that letter! Try again.")
+                return
 
-        # Check if this is a correct letter
-        if letter in self.word:
-            print("Congrats! You got a letter.")
-        # If this is not a correct guess, increment incorrect guesses
-        else:
-            self.incorrect_guesses += 1
-            print("Unlucky, try again!")
+            # Add the current guess to the list of guessed letters
+            self.guessed_letters += letter
+
+            # Check if this is a correct letter
+            if letter in self.word:
+                print("Congrats! You got a letter.")
+            # If this is not a correct guess, increment incorrect guesses
+            else:
+                self.incorrect_guesses += 1
+                print("Unlucky, try again!")
+
+    Hangman().start_game()
+
+    restart = input("Enter 'Y' if you'd like to play again.")
+    if restart == "Y":
+        main()
+    else:
+        exit()
 
 
-Hangman().start_game()
+main()

628aa91e809df844434a6e6ed11802bc48f29235
MishaLea <mishafreestone@ymail.com>
2022-01-27 07:04:37 +0000

    Delete commented out section

--- run.py
+++ run.py
@@ -18,9 +18,6 @@ class Hangman:
     incorrect_guesses = 0
     guessed_letters = []
 
-    # username = input("Enter your name to start: ")
-    # print("Username is: " + username)
-
     def start_game(self):
         """
             Method to start the game.
